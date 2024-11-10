7220197fa44167e816c08837370858e17f6c013a
Wren Morozova <siliconself@proton.me>
2024-11-09 22:28:23 -0700

    Moved CSS generation to correct build script

---
+++ build.rs
@@ -0,0 +1,12 @@
+use std::process::Command;
+
+fn main() {
+    println!("cargo::rerun-if-changed=input.css");
+    println!("cargo::rerun-if-changed=tailwind.config.js");
+    Command::new("npx")
+        .arg("tailwindcss")
+        .args(["-i", "./input.css"])
+        .args(["-o", "./assets/style.css"])
+        .output()
+        .expect("Failed to run Tailwind");
+}
--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -3,13 +3,5 @@
 use std::process::Command;
 
 fn main() {
-    println!("cargo::rerun-if-changed=input.css");
-    println!("cargo::rerun-if-changed=tailwind.config.js");
-    Command::new("npx")
-        .arg("tailwindcss")
-        .args(["-i", "./input.css"])
-        .args(["-o", "./assets/style.css"])
-        .output()
-        .expect("Failed to run Tailwind");
     tauri_build::build();
 }
